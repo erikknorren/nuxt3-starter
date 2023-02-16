e6c52ab5e756aa9e2442b8609fc82f686e125e93
Erik <erikknorren@hotmail.com>
2023-02-16 22:38:19 +0100

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    "no-console": "off",
+    "import/default": "off",
+    "import/no-named-as-default-member": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-multiple-template-root": "off",
+    "vue/attribute-hyphenation": "off",
+  },
+};
