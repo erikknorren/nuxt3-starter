f16823245f993bd1c403d727680c2c5fb7192e5f
Erik <erikknorren@hotmail.com>
2023-02-16 22:44:24 +0100

    middleware

--- server/middleware/middleware.ts
+++ server/middleware/middleware.ts
@@ -1,22 +1,22 @@
-import { server } from "~/server/server";
+import { server } from '~/server/server'
 
-const publicRoutes: string[] = [];
+const publicRoutes: string[] = []
 
 export default defineEventHandler((event) => {
-  console.log("New request: " + event.path);
-  const headers = getRequestHeaders(event);
+  console.log('New request: ' + event.path)
+  const headers = getRequestHeaders(event)
   // Server Initialization
-  server();
+  server()
   // API Middleware
-  if (event.path?.startsWith("/api/")) {
+  if (event.path?.startsWith('/api/')) {
     if (publicRoutes.includes(event.path)) {
-      console.log("Public route, no authorization required");
-      return eventHandler((event) => ({ url: event.path }));
+      console.log('Public route, no authorization required')
+      return eventHandler((event) => ({ url: event.path }))
     }
-    if (headers["x-api-key"] === process.env.API_KEY) {
-      console.log("API key provided");
-      return eventHandler((event) => ({ url: event.path }));
+    if (headers['x-api-key'] === process.env.API_KEY) {
+      console.log('API key provided')
+      return eventHandler((event) => ({ url: event.path }))
     }
-    return sendError(event, createError({ statusCode: 401, statusMessage: "Unauthorized" }));
+    return sendError(event, createError({ statusCode: 401, statusMessage: 'Unauthorized' }))
   }
-});
+})
