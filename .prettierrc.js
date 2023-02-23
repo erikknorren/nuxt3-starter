90fa04c84668742db0dbfc35508a4bed66ae17e3
Erik <erikknorren@hotmail.com>
2023-02-23 15:10:42 +0100

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  printWidth: 150,
+  printWidth: 120,
   tabWidth: 2,
-};
+  bracketSameLine: true,
+  htmlWhitespaceSensitivity: 'ignore',
+}
