b6316e45381894397b08954d7d48df44667862cc
Erik <erikknorren@hotmail.com>
2023-04-20 08:57:58 +0200

    api endpoint

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,6 +1,16 @@
-export default defineEventHandler((event) => {
-  return {
-    url: event.node.req.url,
-    method: event.node.req.method,
+export default defineEventHandler(async (event) => {
+  try {
+    return {
+      statusCode: 200,
+      statusMessage: 'OK',
+      url: event.node.req.url,
+      method: event.node.req.method,
+      query: getQuery(event),
+      headers: getRequestHeaders(event),
+      body: await readBody(event),
+    }
+  } catch (error) {
+    console.log(error)
+    return sendError(event, createError({ statusCode: 500, statusMessage: 'Internal Server Error' }))
   }
 })
