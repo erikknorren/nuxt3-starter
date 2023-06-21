c81358b420fc5371ed7e35840f01de08f6fa98c6
Erik <erikknorren@hotmail.com>
2023-06-21 10:06:58 +0200

    server

--- server/plugins/server.ts
+++ server/plugins/server.ts
@@ -1,3 +1,4 @@
-export default defineNitroPlugin((nitroApp) => {
-  console.log('Starting Nitro server with h3 options:', nitroApp.h3App.options)
+export default defineNitroPlugin(async (nitroApp) => {
+  console.log('Starting Nitro server:', (await $fetch('/api/test', { method: 'POST' })).statusCode === 200 ? 'responding' : 'not responding')
+  return nitroApp
 })
