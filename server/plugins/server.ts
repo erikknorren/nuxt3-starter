7fb37d63b1b519facb165d41e77bc596872a181c
Erik <erikknorren@hotmail.com>
2023-09-05 13:58:20 +0200

    server

--- server/plugins/server.ts
+++ server/plugins/server.ts
@@ -1,6 +1,6 @@
 export default defineNitroPlugin(async (nitroApp) => {
   try {
-    console.log('Nitro server listening on ' + process.env.NUXT_PUBLIC_URL + (process.env.PORT ? ':' + process.env.PORT : ''))
+    await console.log('Nitro server listening on ' + process.env.NUXT_PUBLIC_URL + (process.env.PORT ? ':' + process.env.PORT : ''))
     const testResponse = await $fetch('/api/test', { method: 'POST' })
     console.log('testResponse:', testResponse.statusCode, testResponse.statusMessage)
     return nitroApp
