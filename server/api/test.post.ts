ec3c1172a7499c5300bb74bd39d7a696be21d38f
Erik <erikknorren@hotmail.com>
2023-10-12 08:29:47 +0200

    test

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,16 +1,11 @@
 export default defineEventHandler(async (event) => {
-  try {
-    return {
-      statusCode: 200,
-      statusMessage: 'OK',
-      url: event.node.req.url,
-      method: event.node.req.method,
-      query: getQuery(event),
-      headers: getRequestHeaders(event),
-      body: await readBody(event),
-    }
-  } catch (error) {
-    console.log(error)
-    return sendError(event, createError({ statusCode: 500, statusMessage: 'Internal Server Error' }))
+  return {
+    statusCode: 200,
+    statusMessage: 'OK',
+    url: event.node.req.url,
+    method: event.node.req.method,
+    query: getQuery(event),
+    headers: getRequestHeaders(event),
+    body: await readBody(event),
   }
 })
