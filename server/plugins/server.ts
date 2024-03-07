2bf6488b91ec0486994271114c85bbaf83a00c83
Erik Knorren <86828401+erikknorren@users.noreply.github.com>
2024-03-07 07:56:44 +0100

    server

--- server/plugins/server.ts
+++ server/plugins/server.ts
@@ -1,6 +1,6 @@
 export default defineNitroPlugin(async (nitroApp) => {
   try {
-    await console.log('Nitro server listening on ' + process.env.NUXT_PUBLIC_URL + (process.env.PORT ? ':' + process.env.PORT : ''))
+    await console.log('Nitro server listening on ' + process.env.NUXT_PUBLIC_URL)
     const testResponse = await $fetch('/api/test', { method: 'POST', headers: { 'x-api-key': process.env.NUXT_API_KEY || '' } })
     console.log('testResponse:', testResponse.statusCode, testResponse.statusMessage)
     return nitroApp
